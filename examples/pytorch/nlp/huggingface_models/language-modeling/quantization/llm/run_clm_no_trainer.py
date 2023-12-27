e465cf33125b3082848091dd64c397f05e094901
mengfeil <mengfei.li@intel.com>
2023-12-27 15:24:56 +0800

    enable gptq launch

--- examples/pytorch/nlp/huggingface_models/language-modeling/quantization/llm/run_clm_no_trainer.py
+++ examples/pytorch/nlp/huggingface_models/language-modeling/quantization/llm/run_clm_no_trainer.py
@@ -50,8 +50,7 @@ parser.add_argument("--pad_max_length", default=512, type=int,
                     help="Pad input ids to max length.")
 parser.add_argument("--calib_iters", default=512, type=int,
                     help="calibration iters.")
-parser.add_argument("--tasks", nargs='+', default=["lambada_openai",
-                                                   "hellaswag", "winogrande", "piqa", "wikitext"],
+parser.add_argument("--tasks", nargs='+', default=["lambada_openai", "hellaswag","winogrande","piqa","hendrycksTest-abstract_algebra","hendrycksTest-anatomy","hendrycksTest-astronomy","hendrycksTest-business_ethics","hendrycksTest-clinical_knowledge","hendrycksTest-college_biology","hendrycksTest-college_chemistry","hendrycksTest-college_computer_science","hendrycksTest-college_mathematics","hendrycksTest-college_medicine","hendrycksTest-college_physics","hendrycksTest-computer_security","hendrycksTest-conceptual_physics","hendrycksTest-econometrics","hendrycksTest-electrical_engineering","hendrycksTest-elementary_mathematics","hendrycksTest-formal_logic","hendrycksTest-global_facts","hendrycksTest-high_school_biology","hendrycksTest-high_school_chemistry","hendrycksTest-high_school_computer_science","hendrycksTest-high_school_european_history","hendrycksTest-high_school_geography","hendrycksTest-high_school_government_and_politics","hendrycksTest-high_school_macroeconomics","hendrycksTest-high_school_mathematics","hendrycksTest-high_school_microeconomics","hendrycksTest-high_school_physics","hendrycksTest-high_school_psychology","hendrycksTest-high_school_statistics","hendrycksTest-high_school_us_history","hendrycksTest-high_school_world_history","hendrycksTest-human_aging","hendrycksTest-human_sexuality","hendrycksTest-international_law","hendrycksTest-jurisprudence","hendrycksTest-logical_fallacies","hendrycksTest-machine_learning","hendrycksTest-management","hendrycksTest-marketing","hendrycksTest-medical_genetics","hendrycksTest-miscellaneous","hendrycksTest-moral_disputes","hendrycksTest-moral_scenarios","hendrycksTest-nutrition","hendrycksTest-philosophy","hendrycksTest-prehistory","hendrycksTest-professional_accounting","hendrycksTest-professional_law","hendrycksTest-professional_medicine","hendrycksTest-professional_psychology","hendrycksTest-public_relations","hendrycksTest-security_studies","hendrycksTest-sociology","hendrycksTest-us_foreign_policy","hendrycksTest-virology","hendrycksTest-world_religions","truthfulqa_mc","arc_challenge","wikitext"],
                     type=str, help="tasks list for accuracy validation")
 parser.add_argument("--peft_model_id", type=str, default=None, help="model_name_or_path of peft model")
 # ============SmoothQuant configs==============
@@ -174,7 +173,7 @@ def get_user_model():
         trust_remote_code=args.trust_remote_code,
         revision=args.revision,
     )
-    tokenizer = AutoTokenizer.from_pretrained(args.model)
+    tokenizer = AutoTokenizer.from_pretrained(args.model, trust_remote_code=args.trust_remote_code)
     if args.approach == 'weight_only':
         user_model = user_model.float()
 
@@ -283,7 +282,7 @@ if args.quantize:
             results = evaluate(
                 model="hf-causal",
                 model_args='pretrained=' + args.model + ',tokenizer=' + args.model + ',dtype=float32',
-                user_model=q_model_gptq_debug, tasks=["lambada_openai"],
+                user_model=q_model_gptq_debug, tasks=args.tasks,
                 batch_size=4
             )
             exit(0)
